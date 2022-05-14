95d14785cb1f423f3e955836ae626442c68f1e24
Michael Toohig <michael.toohig@gmail.com>
2022-05-14 12:17:53 +1100

    Fixes #6 transparent div for noServiceWorker alert was blocking click events behind.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -28,49 +28,48 @@
     </v-main>
 
     <div v-else>
+      <div
+        v-if="noServiceWorker"
+        style="pointer-events: none; position: fixed; z-index: 3001; width: 100%;"
+      >
+        <div class="d-flex justify-center">
+          <v-alert
+            style="pointer-events: auto;"
+            class="my-5"
+            max-width="600"
+            type="warning"
+            dark
+            :prominent="!$vuetify.breakpoint.xsOnly"
+            transition="scale-transition"
+            dismissible
+          >
+            <div class="title">
+              {{ $t('app.no_service_worker_title') }}
+            </div>
+            <div v-html="$t('app.no_service_worker_body')" />
+          </v-alert>
+          <v-alert
+            v-if="updateExists"
+            class="mx-auto"
+            max-width="450"
+            type="info"
+            dark
+            :prominent="!$vuetify.breakpoint.xsOnly"
+            transition="scale-transition"
+          >
+            <v-row align="center">
+              <v-col class="grow">
+                {{ $t('app.update_available_title') }}
+              </v-col>
+              <v-col class="shrink">
+                <v-btn @click="refreshApp">{{ $t('app.update_available_btn') }}</v-btn>
+              </v-col>
+            </v-row>
+          </v-alert>
+        </div>
+      </div>
       <SideBar></SideBar>
       <v-main>
-        <div
-          style="position: fixed; z-index: 3001; width: 100%;"
-        >
-          <v-row class="mt-2 mx-0">
-            <v-col justify="center">
-              <v-alert
-                v-if="noServiceWorker"
-                class="mx-auto"
-                max-width="600"
-                type="warning"
-                dark
-                :prominent="!$vuetify.breakpoint.xsOnly"
-                transition="scale-transition"
-                dismissible
-              >
-                <div class="title">
-                  {{ $t('app.no_service_worker_title') }}
-                </div>
-                <div v-html="$t('app.no_service_worker_body')" />
-              </v-alert>
-              <v-alert
-                v-if="updateExists"
-                class="mx-auto"
-                max-width="450"
-                type="info"
-                dark
-                :prominent="!$vuetify.breakpoint.xsOnly"
-                transition="scale-transition"
-              >
-                <v-row align="center">
-                  <v-col class="grow">
-                    {{ $t('app.update_available_title') }}
-                  </v-col>
-                  <v-col class="shrink">
-                    <v-btn @click="refreshApp">{{ $t('app.update_available_btn') }}</v-btn>
-                  </v-col>
-                </v-row>
-              </v-alert>
-            </v-col>
-          </v-row>
-        </div>
         <router-view></router-view>
       </v-main>
       <Notifications></Notifications>
